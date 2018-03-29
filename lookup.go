f3908ec908357210a5f0e0aee913224631f846ce
Aaron O'Mullan <aaron.omullan@gmail.com>
2018-03-29 16:14:36 +0200

    Add .IsCompressibleContentType()

--- lookup.go
+++ lookup.go
@@ -1,5 +1,11 @@
 package mimedb
 
+var isCompressibleDB map[string]bool
+
+func init() {
+	isCompressibleDB = genCompressibleDB()
+}
+
 // ContentType returns the content-type for a given extension, an empty string if non found
 func ContentType(extension string) string {
 	entry := lookupEntry(extension)
@@ -7,6 +13,22 @@ func ContentType(extension string) string {
 		return ""
 	}
 	return entry.ContentType
+}
+
+// genCompressibleDB
+func genCompressibleDB() map[string]bool {
+	cdb := map[string]bool{}
+	for _, entry := range DB {
+		if entry.Compressible {
+			cdb[entry.ContentType] = true
+		}
+	}
+	return cdb
+}
+
+// IsCompressibleContentType returns true if the content-type can be compressed
+func IsCompressibleContentType(contentType string) bool {
+	return isCompressibleDB[contentType]
 }
 
 // IsCompressible if the extension usually belongs to a file that can be compressed
